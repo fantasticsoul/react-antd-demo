54b34d9f9fb12192cd587c4426c9bb2067108a6c
laifeipeng <1020103647@qq.com>
2020-03-10 18:51:58 +0800

    feat: 引入antd和react-app-rewired
     引入antd并且自动加载样式；使用react-app-rewired覆盖默认配置

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button type="primary">Button</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
