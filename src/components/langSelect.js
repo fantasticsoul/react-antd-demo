d1def069c02ee7ecf2a6e7e332c6bf9f81c62b89
laifeipeng <1020103647@qq.com>
2020-03-12 22:33:22 +0800

    feat： 语言是属于全局状态，使用concent实现

--- src/components/langSelect.js
+++ src/components/langSelect.js
@@ -3,24 +3,23 @@ import { Menu, Dropdown } from 'antd'
 import { CaretDownFilled } from '@ant-design/icons'
 import { supportLangs } from '../config'
 import Storage from '../utils/Storage'
+import { selectedLang } from '../utils/StorageTypes'
 import { register } from 'concent'
 
-@register('foo')
+@register('app')
 class LangSelect extends React.Component {
     state = {
-        lang: supportLangs[0],
-        bar: 200,
+        lang: supportLangs[0], // 其实这里的lang会被全剧状态的lang覆盖！
     }
 
     handleMenuClick = e => {
         console.log(e)
         this.setState({ lang: supportLangs[e.key] })
         // TODO 存到localStorage，并存到全局状态
-        Storage.set('selectedLang', supportLangs[e.key])
+        Storage.set(selectedLang, supportLangs[e.key])
     }
 
     render() {
-        const { lang, foo, bar } = this.state
         const menu = (
             <Menu onClick={this.handleMenuClick}>
                 {supportLangs.map((lang, idx) => (
@@ -32,9 +31,6 @@ class LangSelect extends React.Component {
             <Dropdown overlay={menu} trigger="click">
                 <a className="ant-dropdown-link" href="#/" onClick={e => e.preventDefault()}>
                     {this.state.lang.text}
-                    {lang}
-                    {foo}
-                    {bar}
                     <CaretDownFilled style={{ fontSize: '12px' }} />
                 </a>
             </Dropdown>
--- src/models/app/state.js
+++ src/models/app/state.js
@@ -1,6 +1,15 @@
+import Storage from '../../utils/Storage'
+import { selectedLang } from '../../utils/StorageTypes'
+import { supportLangs } from '../../config'
+
+// 用户选择>浏览器默认>项目默认 （浏览器默认不属于我们支持语言的时候才使用项目默认）
+const userLang = Storage.get(selectedLang)
+const browserLang = supportLangs.find(o => o.locale.includes(window.navigator.language))
+const lang = userLang || browserLang || supportLangs[0]
+
 export function getInitialState() {
     return {
-        lang: '',
+        lang: lang,
         foo: 100,
     }
 }
