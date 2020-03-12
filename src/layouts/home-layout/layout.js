fb10bc5f22b55fdd823f2539efb6fabec4b8a566
laifeipeng <1020103647@qq.com>
2020-03-12 11:31:37 +0800

    feat: nav

--- src/layouts/home-layout/layout.js
+++ src/layouts/home-layout/layout.js
@@ -9,21 +9,26 @@ import Footer from './footer'
 import './index.less'
 
 export default function homeLayout({ routes }) {
-  return (
-    <div className={'home-layout'}>
-      <Header></Header>
-      <Nav></Nav>
+    return (
+        <div className={'home-layout'}>
+            <div className={'header-bg'}>
+                <Header></Header>
+            </div>
+            <div className={'nav-bg'}>
+                <Nav></Nav>
+            </div>
+            {/* /home/index：轮播图；/home/tutorial：游戏教程 */}
+            <section className={'home-content'}>
+                <Switch>
+                    {routes.map((route, i) => (
+                        <RouteWithSubRoutes key={i} {...route} />
+                    ))}
+                </Switch>
+            </section>
 
-      {/* /home/index：轮播图；/home/tutorial：游戏教程 */}
-      <section className={'home-content'}>
-        <Switch>
-          {routes.map((route, i) => (
-            <RouteWithSubRoutes key={i} {...route} />
-          ))}
-        </Switch>
-      </section>
-
-      <Footer></Footer>
-    </div>
-  )
+            <div className={'footer-bg'}>
+                <Footer></Footer>
+            </div>
+        </div>
+    )
 }
--- src/layouts/home-layout/nav.js
+++ src/layouts/home-layout/nav.js
@@ -1,51 +1,67 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 import { trontrade, whitePaper, roadmap, telegram, twitter } from '../../config/index'
 
 export default function nav() {
-  return (
-    <nav className={'nav'}>
-      <ul className={'nav-link'}>
-        <li>
-          <Link to="/home">首页</Link>
-        </li>
-        <li>
-          <a href={trontrade} target="_blank" rel="noopener noreferrer" className="hide" title="out-link trontrad">
-            交易所
-          </a>
-        </li>
-        <li>
-          <Link to="/home">FAQ</Link>
-        </li>
-        <li>
-          <a href={whitePaper('en')} target="_blank" rel="noopener noreferrer" title="out-link whitePaper">
-            白皮书
-          </a>
-        </li>
-        <a href={roadmap} target="_blank" rel="noopener noreferrer" title="out-link roadmap">
-          Roadmap
-        </a>
-        <li>
-          <Link to="/home/tutorial">tutorial</Link>
-        </li>
-      </ul>
+    return (
+        <nav className={'nav'}>
+            <ul className={'nav-link'}>
+                <li>
+                    <NavLink to="/home/index">首页</NavLink>
+                </li>
+                <li>
+                    <a
+                        href={trontrade}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="hide"
+                        title="out-link trontrad"
+                    >
+                        交易所
+                    </a>
+                </li>
+                <li>
+                    <Link to="/home">FAQ</Link>
+                </li>
+                <li>
+                    <a
+                        href={whitePaper('en')}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        title="out-link whitePaper"
+                    >
+                        白皮书
+                    </a>
+                </li>
+                <a
+                    href={roadmap}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="out-link roadmap"
+                >
+                    Roadmap
+                </a>
+                <li>
+                    <NavLink to="/home/tutorial">tutorial</NavLink>
+                </li>
+            </ul>
 
-      {/* 联系方式，推特、电报等 */}
-      <ul className={'nav-contact'}>
-        <li>
-          <a href={telegram} target="_blank" rel="noopener noreferrer">
-            telegram
-            <i className="iconfont icon-telegram" style={{ fontSize: '20px' }}></i>
-          </a>
-        </li>
-        <li>
-          <a href={twitter} target="_blank" rel="noopener noreferrer">
-            twitter
-            <i className="iconfont icon-twitter" style={{ fontSize: '20px' }}></i>
-          </a>
-        </li>
-      </ul>
-    </nav>
-  )
+            {/* 联系方式，推特、电报等 */}
+            <ul className={'nav-contact'}>
+                <li>
+                    <a href={telegram} target="_blank" rel="noopener noreferrer">
+                        telegram
+                        <i className="iconfont icon-telegram" style={{ fontSize: '20px' }}></i>
+                    </a>
+                </li>
+                <li>
+                    <a href={twitter} target="_blank" rel="noopener noreferrer">
+                        twitter
+                        <i className="iconfont icon-twitter" style={{ fontSize: '20px' }}></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
 }
